13031af610fced446d89fdec3644424f25affbcb
Sam Laxton <samlaxton@Sams-MacBook-Air-2.local>
2023-02-21 19:05:27 +0000

    header now showing

--- my-app/src/components/PortfolioContainer.js
+++ my-app/src/components/PortfolioContainer.js
@@ -29,6 +29,7 @@ export default function PortfolioContainer() {
 
   return (
     <div>
+      <Header/>
       {/* We are passing the currentPage from state and the function to update it */}
       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
       {/* Here we are calling the renderPage method which will return a component  */}
