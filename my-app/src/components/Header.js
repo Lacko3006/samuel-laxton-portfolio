de59bc6d74b0c4dbdf9b26de509fc694f13ee887
Sam Laxton <samlaxton@Sams-MacBook-Air-2.local>
2023-02-23 18:33:48 +0000

    added LinkedIn logo and bootstrap to header

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,5 +1,24 @@
 import * as React from "react";
+import Image from "../images/LinkedIn_logo_initials.png.webp"
+
+const styles = {
+    card: {
+      // alignItems: "center",
+      background: "#63C6DE",
+      // padding: "10px"
+      // justifyContent
+    },
+    mobile: {
+      fontSize: "20px",
+    }
+}
 
 export default function Header() {
-  return <h1>hello</h1>;
+  return <div className="d-flex flex-row justify-content-between align-items-center" style={styles.card}>
+    <h1>Sam Laxton</h1>
+    <h4 className="" style={styles.mobile} >Mobile: 07584098704</h4>
+    <a href="https://www.linkedin.com/in/samuel-laxton-ab10857b/" target="_blank" rel="noreferrer">
+    <img src={Image} alt="LinkedIn Logo"></img>
+    </a>
+    </div>
 }
