25f9449fa1db839936395cd89d0e7ff5355c6be1
Sam Laxton <samlaxton@Sams-MacBook-Air-2.local>
2023-02-27 20:53:25 +0000

    folder changes

--- src/components/pages/Homepage.js
+++ src/components/pages/Homepage.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Home() {
   return (
     <div>
-      <h1 className='text-center display-1 p-5'>Sam Laxton</h1>
+      <h1 className='text-center display-1 p-5 font-weight-bold'>Sam Laxton</h1>
       <h3 className='text-center display-3 p-3'>
         Junior Software Engineer
       </h3>
