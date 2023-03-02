63e967067a5f4294098260866125a2baaf2ab86e
Sam Laxton <samlaxton@sams-air-2.home>
2023-03-02 20:20:58 +0000

    added About.css

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import Styles from "../../css/Styles.css";
+import AboutCss from "../../css/About.css"
+
+const aboutMeCss = { ...Styles, ...AboutCss }
 
 export default function About() {
   return (
-    <div className="window-size">
+    <div className="window-size about-me">
       <a id="about"></a>
-      <h1>About Me</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+      <h1 className="text-center p-5" >About Me</h1>
+      <h5>
+        I am a recent graduate of the University of Birmingham Coding Bootcamp. This cohort took place after work for 6 months, it taught me many valuable technologies that I loved using.
+      </h5>
     </div>
   );
 }
