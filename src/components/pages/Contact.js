486b9c4aa5c51b101871ef8506c600f9e7e22a1c
Sam Laxton <samlaxton@sams-air-2.home>
2023-03-02 11:10:21 +0000

    added class to contact.js

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Contact() {
   return (
-    <div className='window-size' >
+    <div className='window-sizeg' >
       <a id="contact"></a>
       <h1>Contact Page</h1>
       <p>
