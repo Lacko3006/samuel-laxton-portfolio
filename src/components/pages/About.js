5c7c63b961f91397f697ac3512e341b676477cab
Sam Laxton <samlaxton@sams-air-2.home>
2023-03-28 18:36:10 +0100

    finished about me page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -7,12 +7,15 @@ const aboutMeCss = { ...Styles, ...AboutCss }
 
 export default function About() {
   return (
-    <div className="window-size about-me">
+    <div className="window-size about-me-background">
       <a id="about"></a>
-      <h1 className="text-end p-5 " ><u>About Me</u></h1>
-      <h5 className="about-me-text">
-        I am currently employed at Element Materials Technology as a Doors & Window Test Engineer. I have recently graduated from the University of Birmingham Coding Bootcamp. This cohort took place after work for 24 weeks. It went through a variety of different technologies such as: HTML5, CSS3, JavaScript, jQuery, Bootstrap, Express.js, React.js, Node.js, Database Theory, Bookshelf.js, MongoDB, MySQL, Java, Command Line and Git. 
-      </h5>
+      <h2 className="about-me" >About Me</h2>
+      <p className="about-me-text">
+        I am Sam Laxton. I have recently graduated from the University of Birmingham Coding Bootcamp, finishing this bootcamp has given me a great passion for Web Development and its technologies. This cohort took place after work for 24 weeks. It went through a variety of different technologies such as: HTML5, CSS3, JavaScript, jQuery, Bootstrap, Express.js, React.js, Node.js, Database Theory, Bookshelf.js, MongoDB, MySQL, Java, Command Line and Git.
+      </p>
+      <p className='about-me-text-p2'>
+      I am currently employed at Element Materials Technology as a Doors & Window Test Engineer. I have always wanted a jon in technology because it has always been a passion of mine and being able to be creative in a technology environment is what I wanted to make my career.
+      </p>
       <img className="profile-pic" src={ProfilePic}/>
     </div>
   );
--- src/components/pages/Homepage.js
+++ src/components/pages/Homepage.js
@@ -6,10 +6,10 @@ export default function Home() {
     <div className="window-size">
       <div className="header-name">
         <h1 className="text-center p-5"></h1>
-        <h1 className="text-center display-1 pt-5 font-weight-bold header-name">
+        <h1 className="text-center pt-5 font-weight-bold header-name">
           Sam Laxton
         </h1>
-        <h3 className="text-center display-2">Junior Software Engineer</h3>
+        <h3 className="text-center header-title">Junior Software Engineer</h3>
       </div>
 
     </div>
