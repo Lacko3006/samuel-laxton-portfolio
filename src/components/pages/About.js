29ea0b0fb793f0707986ffb0f69730888ff38364
Sam Laxton <samlaxton@sams-air-2.home>
2023-04-04 17:42:18 +0100

    add mobile changes

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -10,10 +10,10 @@ export default function About() {
       <a id="about"></a>
       <h2 className="about-me" >About Me</h2>
       <p className="about-me-text">
-        I am Sam Laxton. I have recently graduated from the University of Birmingham Coding Bootcamp, finishing this bootcamp has given me a great passion for Web Development and its technologies. This cohort took place after work for 24 weeks. It went through a variety of different technologies such as: HTML5, CSS3, JavaScript, jQuery, Bootstrap, Express.js, React.js, Node.js, Database Theory, Bookshelf.js, MongoDB, MySQL, Java, Command Line and Git.
+        I am Sam Laxton. I have graduated from the University of Birmingham Coding Bootcamp. I have a passion for Web Development and its technologies. I attended the bootcamp 24 week course at the same time as working full time.  I have learnt a variety of different technologies such as: HTML5, CSS3, JavaScript, jQuery, Bootstrap, Express.js, React.js, Node.js, Database Theory, Bookshelf.js, MongoDB, MySQL, Java, Command Line and Git.
       </p>
       <p className='about-me-text-p2'>
-      I am currently employed at Element Materials Technology as a Doors & Window Test Engineer. I have always wanted a jon in technology because it has always been a passion of mine and being able to be creative in a technology environment is what I wanted to make my career.
+      I am currently employed at Element Materials Technology as a Doors & Window Test Engineer. I am now looking for employment in technology. It has always been a passion of mine and being able to be creative in this sector and want to pursue my career
       </p>
       <img className="profile-pic" src={ProfilePic}/>
       </div>
