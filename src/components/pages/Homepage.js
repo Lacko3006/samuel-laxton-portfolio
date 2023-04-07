d12be4f247a3daa93c7e8408ceccc65fef1418eb
Sam Laxton <samlaxton@sams-air-2.home>
2023-04-07 17:32:30 +0100

    removed bootstrap

--- src/components/pages/Homepage.js
+++ src/components/pages/Homepage.js
@@ -4,12 +4,11 @@ import "../../css/Homepage.css"
 export default function Home() {
   return (
     <div className="window-size">
-      <div className="header-name">
-        <h1 className="text-center p-5"></h1>
-        <h1 className="text-center pt-5 font-weight-bold header-name">
+      <div className="homepage">
+        <h1 className="header-name">
           Sam Laxton
         </h1>
-        <h3 className="text-center header-title">Junior Software Engineer</h3>
+        <h3 className="header-title">Junior Software Engineer</h3>
       </div>
 
     </div>
