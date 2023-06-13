607801dada1d3a0ed13e119eb50a1d83de2cd1aa
Sam Laxton <samlaxton@sams-air-2.home>
2023-06-13 17:58:03 +0100

    added links and pictures to homepage for links to github account, linkedIn & resume

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,9 +1,5 @@
 import React from "react";
 import "../../css/Contact.css";
-import GitHubLogo from "../../images/GitHub-logo.jpg";
-import LinkedInLogo from "../../images/LinkedIn_logo_initials.png.webp";
-import ResumeImg from "../../images/resume-pic.jpg";
-import ResumePdf from "../../resume/sam-laxton-cv.pdf";
 
 export default function Contact() {
   return (
@@ -11,40 +7,7 @@ export default function Contact() {
       <div className="contact-background">
         <a id="contact"></a>
         <div>
-          <h1 className="contact-title">Contact Page</h1>
-          <h5 className="link-description">Here are my Social Media Links!</h5>
-          <ul className="links">
-            <li className="">
-              <a href="https://github.com/Lacko3006" target="_blank">
-                <img
-                  className="github-img"
-                  src={GitHubLogo}
-                  alt="GitHub Logo"
-                ></img>
-              </a>
-            </li>
-            <li className="">
-              <a
-                href="https://www.linkedin.com/in/samuel-laxton-ab10857b/"
-                target="_blank"
-              >
-                <img
-                  className="linked-in-img"
-                  src={LinkedInLogo}
-                  alt="LinkedIn Logo"
-                ></img>
-              </a>
-            </li>
-            <li className="">
-              <a href={ResumePdf} target="_blank">
-                <img
-                  className="resume-img"
-                  src={ResumeImg}
-                  alt="Resume Image"
-                ></img>
-              </a>
-            </li>
-          </ul>
+          <h1 className="contact-title">Skills</h1>
         </div>
       </div>
     </div>
--- src/components/pages/Homepage.js
+++ src/components/pages/Homepage.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "../../css/Homepage.css";
+import GitHubLogo from "../../images/GitHub-logo.png";
+import LinkedInLogo from "../../images/LinkedIn_logo_initials.png.webp";
+import ResumeImg from "../../images/resume-pic.jpeg";
+import ResumePdf from "../../resume/sam-laxton-cv.pdf";
 
 export default function Home() {
   return (
@@ -7,6 +11,38 @@ export default function Home() {
       <a id="home"></a>
       <h1 className="homepage-name">Sam Laxton</h1>
       <h3 className="homepage-title">Junior Software Engineer</h3>
+      <ul className="links">
+            <li className="">
+              <a href="https://github.com/Lacko3006" target="_blank">
+                <img
+                  className="github-img"
+                  src={GitHubLogo}
+                  alt="GitHub Logo"
+                ></img>
+              </a>
+            </li>
+            <li className="">
+              <a
+                href="https://www.linkedin.com/in/samuel-laxton-ab10857b/"
+                target="_blank"
+              >
+                <img
+                  className="linked-in-img"
+                  src={LinkedInLogo}
+                  alt="LinkedIn Logo"
+                ></img>
+              </a>
+            </li>
+            <li className="">
+              <a href={ResumePdf} target="_blank">
+                <img
+                  className="resume-img"
+                  src={ResumeImg}
+                  alt="Resume Image"
+                ></img>
+              </a>
+            </li>
+          </ul>
     </div>
   );
 }
